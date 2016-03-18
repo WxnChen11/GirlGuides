6a5ce7d2f0d0b305d104a8de1f547bddfc6db92b
root <wenxinchen11@hotmail.com>
2016-03-18 14:21:40 -0400

    apache server running

---
+++ static/scripts.js
@@ -0,0 +1,24 @@
+/**
+ * Created by winston on 18/03/16.
+ */
+
+
+
+//(function(){var s=document.createElement("script");s.onload=function()
+//{bootlint.showLintReportForCurrentDocument([]);};s.src="https://maxcdn.bootstrapcdn.com/bootlint/latest/bootlint.min.js";document.body.appendChild(s)})();
+
+
+//set height of containers to 90% window height
+var h = $(window).height();
+var w = $(window).width();
+
+$("#mainwin").css("height",h *.65);
+$("#main_container").css("height", h * 0.9);
+
+//get width of jumbotron, set displaytable to 90%
+var wJumbo = $("#main_container").width();
+//$("#displayTable").css("width", wJumbo);
+
+$(".badges").hover(function(){($(this).attr("src", "/badgeSampleHover.png"))},function(){($(this).attr("src", "/badgeSample.png"))});
+
+$(".badges").click(function(){$("#dummybutton").trigger('click'); console.log("click")});
